6b106f9f59cf7dd4d7f2d0991a9def35f6cc0dcb
kishimoto <kishimoto88@sanken.osaka-u.ac.jp>
2023-09-18 15:08:58 +0900

    modify

--- models.py
+++ models.py
@@ -1,9 +1,13 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///commands.db'
+db = SQLAlchemy(app)
 
 class Command(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     keyword = db.Column(db.String(255), nullable=False)
     full_command = db.Column(db.String(255), nullable=False)
 
+db.create_all()
